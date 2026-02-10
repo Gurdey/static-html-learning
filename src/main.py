ca812235b2f66b0b6d8d7cb2d821126f9a49b4b5
Gurdey <grellek@hotmail.ca>
2026-02-10 11:02:34 -0700

    Finished Lesson

--- src/main.py
+++ src/main.py
@@ -3,14 +3,13 @@ import sys
 from functions import clear_public_directory, copy_static_to_public, generate_pages_recursive
 
 def main():
-    dest_path = "docs"
-    clear_public_directory(dest_path)
-    copy_static_to_public(target_dir= dest_path)
+    dest_path = "public"
     basepath = "/"
     if len(sys.argv) > 1:
         basepath = sys.argv[1]
+        dest_path = "docs"
+    clear_public_directory(dest_path)
+    copy_static_to_public(target_dir= dest_path)
     generate_pages_recursive("content", "template.html", dest_path, basepath)
-
-
 
 main()
